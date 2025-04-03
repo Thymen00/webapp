49674533d28447929703123e219b3dec4fc4ef0b
Thymen <fegel.thymen@gmail.com>
2025-04-03 05:54:47 +0200

    color

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 export default function MyApp() {
-  return (<div>
-    <h1>Hello World!!</h1>
-    <p>
+  return (<div className="w-[80%] border rounded p-4 m-auto">
+    <h1 className="text-2xl font-bold m-2 p-20">Hello World!!</h1>
+    <p className="w-100 p-2  m-5 border bg-blue-200">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab qui nam voluptas quidem quam adipisci dignissimos omnis facilis minus error sapiente, laboriosam expedita totam, in quasi aliquid. Laboriosam, deserunt beatae.
-    </p>
-  </div>)
-  
+      </p>
+      <p className="w-100 m-5 border bg-blue-600">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos ipsum dolorem corrupti obcaecati alias ab magni accusantium iste impedit, deleniti quasi, sint perspiciatis reprehenderit at sed officia exercitationem labore odio.
+      </p>
+      <p className="w-100 m-5 border bg-amber-200">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi nulla possimus molestias id sed quasi veritatis ut veniam magni ratione ducimus, eius saepe quia illo odio! Accusamus odit tenetur ad?
+      </p>
+
+      </div>
+  )
 }
