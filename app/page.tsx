442e072c88ceb4dbb496075acba44bce08d38dde
Thymen <fegel.thymen@gmail.com>
2025-04-03 05:36:44 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function MyApp() {
+  return (<div>
+    <h1>Hello World!!</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab qui nam voluptas quidem quam adipisci dignissimos omnis facilis minus error sapiente, laboriosam expedita totam, in quasi aliquid. Laboriosam, deserunt beatae.
+    </p>
+  </div>)
+  
+}
